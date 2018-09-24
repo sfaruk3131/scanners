e2a7c9eb5ee3da43dfe50aeea864ccd79b1476fa
sfaruk3131 <sfaruk3131@bths.edu>
2018-09-24 10:13:23 -0400

    Scanner name and age

--- src/test3.java
+++ src/test3.java
@@ -8,6 +8,11 @@ public class test3
         System.out.println("What is your name");
         String res = input.next();
         System.out.println("Nice to meet you " + res);
+
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("What is your age");
+        int num = input2.nextInt();
+        System.out.println(num);
     }
 
 }
